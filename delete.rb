c7dd792631cd09e7c15f3a8bf6e9b24b1c238f07
Sho Hashimoto <hashimoto@shokai.org>
2011-08-01 04:58:18 +0900

    delete

---
+++ delete.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__)+'/helper'
+require 'FileUtils'
+
+videos = Video.where(:file => /.+/, :video_url => /^http.+/)
+
+puts "#{videos.count} videos in queue"
+
+videos.each{|v|
+  puts "#{v.title} - #{v.url}"
+  puts "  delete? [y/N]"
+  if gets =~ /y/i
+    begin
+      fname = "#{File.dirname(__FILE__)}/#{@@conf['dir']}/#{v.file}"
+      File.delete fname
+      v.delete
+      puts " => #{fname} deleted"
+    rescue => e
+      STDERR.puts e
+    end
+  else
+  end
+}
