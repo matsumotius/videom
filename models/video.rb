73069dfb869863b5fa5b0ce55f425298f2cbcc74
Sho Hashimoto <hashimoto@shokai.org>
2011-08-04 05:05:45 +0900

    add mongoid document fields

--- models/video.rb
+++ models/video.rb
@@ -2,4 +2,10 @@
 class Video
   include Mongoid::Document
   field :error_count, :type => Integer, :default => 0
+  field :title, :type => String
+  field :file, :type => String
+  field :tags, :type => Array
+  field :url, :type => String
+  field :video_url, :type => String
+  field :thumb_gif
 end
