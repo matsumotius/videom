d06b04df390a601eb3dd9011b393c6b28607166b
Sho Hashimoto <hashimoto@shokai.org>
2011-08-26 18:09:16 +0900

    fix make_thumb

--- tools/make_thumbnails.rb
+++ tools/make_thumbnails.rb
@@ -9,6 +9,7 @@ parser.comment(:video2gif, 'video2gif command path')
 parser.comment(:gif_fps, 'gif fps', 1)
 parser.comment(:video_fps, 'video fps', 0.01)
 parser.comment(:size, 'size', '160x90')
+parser.comment(:tmp_dir, 'tmp_dir', '/var/tmp/video2gif')
 parser.bind(:help, :h, 'show help')
 first, params = parser.parse(ARGV)
 
@@ -18,19 +19,23 @@ if parser.has_option(:help) or !parser.has_params([:video2gif])
 end
 
 loop do
-  videos = Video.not_in(:file => [nil]).where(:exif.exists => true, :thumb_gif.exists => false)
-  loop do
-    break if videos.empty?
-    v = videos.shift
-    puts "#{v.title}(id:#{v.id}) - #{videos.count+1} videos in thumbnail queue"
+  videos = Video.where(:file => /.+/, :exif.exists => true, :thumb_gif.exists => false)
+  puts "==#{videos.count+1} videos in thumbnail queue=="
+  for v in videos do
+    puts "#{v.title}(id:#{v.id})"
     file = "#{@@dir}/#{v.file}"
     next unless File.exists? file
     out = "#{@@thumb_dir}/#{v.file}.gif"
-    puts cmd = "#{params[:video2gif]} -i #{file} -o #{out} -video_fps #{params[:video_fps]} -gif_fps #{params[:gif_fps]} -size #{params[:size]}"
-    system cmd
-    next unless File.exists? out
-    v[:thumb_gif] = "#{v.file}.gif"
-    v.save
+    puts cmd = "#{params[:video2gif]} -i #{file} -o #{out} -video_fps #{params[:video_fps]} -gif_fps #{params[:gif_fps]} -size #{params[:size]} -tmp_dir #{params[:tmp_dir]}"
+    begin
+      system cmd
+      next unless File.exists? out
+      v[:thumb_gif] = "#{v.file}.gif"
+      v.save
+    rescue => e
+      STDERR.puts e
+      next
+    end
   end
   break unless params[:loop]
   sleep params[:interval].to_i
