482da5b98cc45d2a67725a29e70e593e2ed4315f
Sho Hashimoto <hashimoto@shokai.org>
2011-09-21 09:28:27 +0900

    search on press enter key

--- player/public/js/index.js
+++ player/public/js/index.js
@@ -1,18 +1,28 @@
 
 $(function(){
-    $('.btn_delete').click(function(e){
-        var video_id = e.currentTarget.id;
-        if(!confirm('delete?')) return;
-        $.del(app_root+'/v/'+video_id, {}, function(e){
-            if(e.error) alert(e.message);
-            else{
-                alert(e.message);
-            }
-        }, 'json')
-    });
+      $('.btn_delete').click(
+          function(e){
+              var video_id = e.currentTarget.id;
+              if(!confirm('delete?')) return;
+              $.del(app_root+'/v/'+video_id, {}, 
+                    function(e){
+                        if(e.error) alert(e.message);
+                        else{
+                            alert(e.message);
+                        }
+                    }, 'json')
+          }
+      );
     
-    $('#btn_search').click(function(){
-        var word = $('#input_search').val();
-        location.href = app_root+'/search/'+word;
-    });
+      var search = function(){
+          var word = $('#input_search').val();
+          location.href = app_root+'/search/'+word;        
+      };
+      
+    $('#btn_search').click(search);
+    $('#input_search').keydown(
+        function(e){
+            if(e.keyCode == 13) search();
+        }
+    );
 });
