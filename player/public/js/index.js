6613233d2263a10c0e526cd9ca1dc00bdee02599
Sho Hashimoto <hashimoto@shokai.org>
2011-11-17 21:10:26 +0900

    disabled delete button

--- player/public/js/index.js
+++ player/public/js/index.js
@@ -1,19 +1,5 @@
 
 $(function(){
-      $('.btn_delete').click(
-          function(e){
-              var video_id = e.currentTarget.id;
-              if(!confirm('delete?')) return;
-              $.del(app_root+'/v/'+video_id, {}, 
-                    function(e){
-                        if(e.error) alert(e.message);
-                        else{
-                            alert(e.message);
-                        }
-                    }, 'json')
-          }
-      );
-    
       var search = function(){
           var word = $('#input_search').val();
           location.href = app_root+'/search/'+word;        
